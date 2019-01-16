9462837b17d9fceee32f36e31a13c5989cce1154
navgg <navgg@mail.ru>
2019-01-16 13:44:42 +0200

    1.32 rail overrides simple rail

--- cgame/cg_weapons.c
+++ cgame/cg_weapons.c
@@ -210,12 +210,12 @@ static void CG_RailTrail32 (clientInfo_t *ci, vec3_t start, vec3_t end) {
 	VectorCopy(move, next_move);
 	VectorScale (vec, SPACING, vec);
 
-	if (cgx_weaponEffects.integer & WE_RAILSIMPLE) {
-		// nudge down a bit so it isn't exactly in center
-		re->origin[2] -= 8;
-		re->oldorigin[2] -= 8;
-		return;
-	}
+	//if (cgx_weaponEffects.integer & WE_RAILSIMPLE) {
+	//	// nudge down a bit so it isn't exactly in center
+	//	re->origin[2] -= 8;
+	//	re->oldorigin[2] -= 8;
+	//	return;
+	//}
 	skip = -1;
 
 	j = 18;
@@ -272,40 +272,41 @@ void CG_RailTrail( clientInfo_t *ci, vec3_t start, vec3_t end ) {
 	localEntity_t	*le;
 	refEntity_t		*re;
 
-	if (cgx_weaponEffects.integer & WE_RAIL32 || cgx_weaponEffects.integer & WE_RAILSIMPLE) {
+	if (cgx_weaponEffects.integer & WE_RAIL32) {
 		CG_RailTrail32(ci, start, end);
 		return;
 	}
 
-	//
-	// rings
-	//
-	le = CG_AllocLocalEntity();
-	re = &le->refEntity;
+	if (!(cgx_weaponEffects.integer & WE_RAILSIMPLE)) {
+		//
+		// rings
+		//
+		le = CG_AllocLocalEntity();
+		re = &le->refEntity;
 
-	le->leType = LE_FADE_RGB;
-	le->startTime = cg.time;
-	le->endTime = cg.time + cg_railTrailTime.value;
-	le->lifeRate = 1.0 / ( le->endTime - le->startTime );
+		le->leType = LE_FADE_RGB;
+		le->startTime = cg.time;
+		le->endTime = cg.time + cg_railTrailTime.value;
+		le->lifeRate = 1.0 / ( le->endTime - le->startTime );
 
-	re->shaderTime = cg.time / 1000.0f;
-	re->reType = RT_RAIL_RINGS;
-	re->customShader = cgs.media.railRingsShader;
+		re->shaderTime = cg.time / 1000.0f;
+		re->reType = RT_RAIL_RINGS;
+		re->customShader = cgs.media.railRingsShader;
 	
-	VectorCopy( start, re->origin );
-	VectorCopy( end, re->oldorigin );
+		VectorCopy( start, re->origin );
+		VectorCopy( end, re->oldorigin );
 
-	// nudge down a bit so it isn't exactly in center
-	re->origin[2] -= 8;
-	re->oldorigin[2] -= 8;
+		// nudge down a bit so it isn't exactly in center
+		re->origin[2] -= 8;
+		re->oldorigin[2] -= 8;
 
-	le->color[0] = ci->color[0] * 0.75;
-	le->color[1] = ci->color[1] * 0.75;
-	le->color[2] = ci->color[2] * 0.75;
-	le->color[3] = 1.0f;
+		le->color[0] = ci->color[0] * 0.75;
+		le->color[1] = ci->color[1] * 0.75;
+		le->color[2] = ci->color[2] * 0.75;
+		le->color[3] = 1.0f;
 
-	AxisClear( re->axis );
-
+		AxisClear( re->axis );
+	}
 	//
 	// core
 	//
